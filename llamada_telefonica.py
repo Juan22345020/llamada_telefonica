df40640f08f29c2c30811cc8e402db517238c2e8
Juan22345020 <126175834+Juan22345020@users.noreply.github.com>
2023-03-04 08:45:45 -0500

    Add files via upload

---
+++ llamada_telefonica.py
@@ -0,0 +1,34 @@
+# Valor llamada telefonica
+
+print("----------------------------------------------------------")
+print("---------------VALOR LLAMADA TELEFONICA-------------------")
+print("----------------------------------------------------------")
+
+
+# input
+
+dl = int(input("Digite la duracion de las llamada: " ))
+
+## procesing
+
+if dl<=3 :
+   cl=300
+else:
+   cl=300+50*(dl-3)
+
+
+
+
+
+   
+ 
+
+
+
+
+print("el costo de la llamada es: " + str(cl) )
+
+
+   
+
+
